0156ce237f2b5a7114de1b88c9ae0c41ecacb478
Nathaniel Quansah <n.bongo40@gmail.com>
2025-01-01 14:50:29 +0000

    added analytics

--- v2/src/app/layout.tsx
+++ v2/src/app/layout.tsx
@@ -1,3 +1,6 @@
+// imports
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 // styles
 import "@/styles/globals.css";
 
@@ -17,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${bricolageGrotesque.variable} antialiased`}>
+      <body
+        className={`${poppins.variable} ${bricolageGrotesque.variable} antialiased`}
+      >
         <div className="relative min-h-screen h-min w-auto pt-40 flex flex-col flex-nowrap overflow-visible">
           <div className="contents">
             <Navbar />
@@ -26,6 +31,7 @@ export default function RootLayout({
           </div>
         </div>
       </body>
+      <GoogleAnalytics gaId={process.env.FIREBASE_MEASUREMENT_ID!} />
     </html>
   );
 }
